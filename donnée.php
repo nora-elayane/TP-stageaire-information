1da5a39748f48143d87d680db6c18669d694e63d
Nora <noraelayane44@gmail.com>
2026-04-25 14:04:35 +0200

    modif

--- donnée.php
+++ donnée.php
@@ -1,38 +1,37 @@
 <?php
-if(!isset($_POST["nom"] , $_POST["prenom"] , $_POST["date"] , $_POST["date"] , $_POST["ville"] , $_POST["cin"] , $_POST["tele"])){
-    header("location:index.php") ;
-}else{
-    $dateAct = new DateTime() ; 
-    $dateN = new DateTime($_POST["date"]) ;
+if (!isset($_POST["nom"], $_POST["prenom"], $_POST["date"], $_POST["ville"], $_POST["cin"], $_POST["tele"])) {
+    header("location:index.php?msg=valeur invalid");
+    exit();
+} else {
+    $dateAct = new DateTime();
+    $dateN = new DateTime($_POST["date"]);
     $age = $dateAct->diff($dateN);
-    $age = $age->y ;
-    $img = $_FILES["img"] ;
-    $finfo = new finfo(FILEINFO_MIME_TYPE) ;
+    $age = $age->y;
+    $img = $_FILES["img"];
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
     // echo $img['temp_name'] ;
     $mime = $finfo->file($_FILES['img']['tmp_name']);
-    $exten = ['image/jpeg', 'image/png', 'image/gif'] ;
-    if(!in_array($mime , $exten)){
-        header("location:index.php?msg=Extention invalid") ;
-        exit();     
-    }
-    if($img['size'] > 2*1024*1024){
-        header("location:index.php?msg=SIZE!!") ;
+    $exten = ['image/jpeg', 'image/png', 'image/gif'];
+    if (!in_array($mime, $exten)) {
+        header("location:index.php?msg=Extention invalid");
         exit();
     }
-    $ext  = ['image/jpeg'=>'.jpg', 'image/png'=>'.png', 'image/gif'=>'.gif'];
-    $nomImage = uniqid() . $ext[$mime] ;
+    if ($img['size'] > 2 * 1024 * 1024) {
+        header("location:index.php?msg=SIZE!!");
+        exit();
+    }
+    $ext  = ['image/jpeg' => '.jpg', 'image/png' => '.png', 'image/gif' => '.gif'];
+    $nomImage = uniqid() . $ext[$mime];
     $dest = __DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $nomImage;
-    move_uploaded_file($_FILES['img']['tmp_name'], $dest) ;
-    $nomImage = "<img src='images/{$nomImage}' width='50px '>" ;
-    $string = $_POST["nom"] . ";" . $_POST["prenom"] . ";" . $_POST["date"] .";" . $age . ";" . $_POST["ville"] . ";" . $_POST["cin"] . ";" . $_POST["tele"] . ";" . $nomImage . "\n" ;
-    $fichier = fopen("stageaire.txt" , "a") ;
-    file_put_contents("stageaire.txt" , $string , FILE_APPEND) ;
-    fclose($fichier) ;
-    $contenu = file_get_contents("stageaire.txt") ;
+    move_uploaded_file($_FILES['img']['tmp_name'], $dest);
+    $nomImage = "<img src='images/{$nomImage}' width='50px '>";
+    $string = $_POST["nom"] . ";" . $_POST["prenom"] . ";" . $_POST["date"] . ";" . $age . ";" . $_POST["ville"] . ";" . $_POST["cin"] . ";" . $_POST["tele"] . ";" . $nomImage . "\n";
+    file_put_contents("stageaire.txt", $string, FILE_APPEND);
+    $contenu = file_get_contents("stageaire.txt");
     // echo $contenu ; 
-    $new = explode("\n" , $contenu);
+    $new = explode("\n", $contenu);
     // print_r ($new) ;
-    echo "<style>td{border : 1px black solid ; padding:5px ;}</style>" ;
+    echo "<style>td{border : 1px black solid ; padding:5px ;}</style>";
     echo "<div class='container'>";
     echo "<h2>Liste des stagiaires</h2>";
     echo "<table style='border-collapse: collapse;'>
@@ -45,18 +44,19 @@ if(!isset($_POST["nom"] , $_POST["prenom"] , $_POST["date"] , $_POST["date"] , $
     <td>cin</td>
     <td>tele</td>
     <td>image</td>
-    </tr>" ; 
+    </tr>";
     foreach ($new as  $value) {
-        echo "<tr>";
-        $l =  explode(";" ,$value) ;
-        foreach ($l as  $v) {
-        echo "<td>{$v}</td>";
+        if (!empty($value)) {
+            echo "<tr>";
+            $l =  explode(";", $value);
+            foreach ($l as  $v) {
+                echo "<td>{$v}</td>";
+            }
+            echo "</tr>";
         }
-        echo "</tr>" ;  
     }
-
 }
-echo "</table>" ; 
+echo "</table>";
 echo "</div>";
 echo "
 <style>
@@ -130,5 +130,4 @@ a:hover {
 </style>
 ";
 
-echo "<a href='index.php' style='text-decoration:none; color:white; background-color:grey; padding:6px ; border-raduis:15px ;'>Ajouter un autre stageaire</a>" ;
-?>
+echo "<a href='index.php' style='text-decoration:none; color:white; background-color:grey; padding:6px ; border-raduis:15px ;'>Ajouter un autre stageaire</a>";
